c5a9fb1dfa098b3865583964ed8ecf4a14a60994
hamidreza <lolaeihamidreza@gmail.com>
2022-03-02 14:04:37 +0330

    b2b13

--- Products/urls.py
+++ Products/urls.py
@@ -5,5 +5,8 @@ app_name = 'Products'
 
 urlpatterns =[
        path('', views.products_list_page, name = 'product_list_page'),
-       path('<slug:slug>/', views.product_detail_page, name = 'product_detail_page')
+       path('<slug:slug>/', views.product_detail_page, name = 'product_detail_page'), 
+       path('add', views.add_product, name = 'add_product'), 
+       path('remove', views.remove_product, name='remove_product'), 
+       path('invalid-user', views.invalid_user, name = 'invalid_user')
 ]
--- Products/views.py
+++ Products/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import get_object_or_404, render
-from .models import Products, ProductsComments
+from http.client import HTTPResponse
+from django.shortcuts import get_object_or_404, redirect, render
+from .models import Products, ProductsComments, ProductCategories
 from django.core.paginator import Paginator
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from Sellers.models import Sellers 
 
 """
 Views :
 
     product_list() # View all products per page
     product_detail() # View details of every product
+    add_product() # product is added by seller
+    remove_product() # prodcut is removed by seller
     
 """
 
@@ -20,6 +25,7 @@ def products_list_page(request):
         'pages' : pages
         }
     return render(request, 'Products/products_list_page/products_list_page.html', context )
+
 
 
 def product_detail_page(request, slug):
@@ -39,3 +45,59 @@ def product_detail_page(request, slug):
         'comments' : comments
     }
     return render(request, 'Products/product_detail_page/product_detail_page.html', context)
+
+
+
+def add_product(request):
+    seller_user = Sellers.objects.filter(business_owner=request.user).filter(business_status=True)
+    if not seller_user:
+        return redirect('http://127.0.0.1:8000/product/invalid-user')
+    p_category = ProductCategories.objects.all()
+    if request.method == 'POST':
+        title = request.POST['title']
+        description = request.POST['description']  
+        price = request.POST['price']  
+        inventory = request.POST['inventory']
+        category = p_category.object.get(name = request.POST['category'])
+        seller = seller_user
+        p_data = Products.objects.create(
+                        title=title, 
+                        description=description, 
+                        price=price, 
+                        inventory=inventory, 
+                        category=category, 
+                        seller = seller
+                        )
+        p_data.save()
+    else : 
+        HttpResponse(request)
+    return render(request, 'Products/add_product/add_product.html')
+    
+
+
+def remove_product(request, title):
+    seller_user = Sellers.objects.get(business_owner=request.user).filter(business_status=True)
+    if not seller_user:
+        return redirect('http://127.0.0.1:8000/product/invalid-user')
+    product = Products.objects.get(title=title)
+    if product: 
+        product.delete()
+        # return HttpResponseRedirect('detelet_template') # if redirect is needed
+        # return redirect('success template')
+    # else : 
+    #     return redirect('not success template')
+
+
+def invalid_user(request):
+    return render(request, 'Products/add_product/invalid_user.html')
+    
+
+    
+
+
+
+
+    
+
+
+
